8b70677cbff14c0015a4368a856a27e869c11101
huseyin.merkit <huseyin.merkit@itm-techsoft.com>
2021-02-14 17:05:06 +0300

    Upload codes

---
+++ TcpTimeClient/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace Communicator
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    class TcpTimeClient
+    {
+        const int _port = 9999;
+
+        static void Main(string[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint remoteEP = new IPEndPoint(ipAddress, _port);
+            
+            var client = new TcpClient();
+            client.Connect(remoteEP);
+            Console.WriteLine("Client End Point: " + client.Client.LocalEndPoint.ToString());
+
+            NetworkStream ns = client.GetStream();
+            byte[] bytes = new byte[1024];
+            int bytesRead = ns.Read(bytes, 0, bytes.Length);
+            Console.WriteLine(Encoding.ASCII.GetString(bytes, 0, bytesRead));
+            client.Close();
+
+            Console.WriteLine("Press enter to exit program...");
+            Console.ReadLine();
+        }
+    }
+}
---
+++ TcpTimeServer/Program.cs
@@ -0,0 +1,57 @@
+﻿namespace TcpTimeServer
+{
+    using System;
+    using System.Net;
+    using System.Net.Sockets;
+    using System.Text;
+
+    public class TcpTimeServer
+    {
+        const int _port = 9999;
+
+        public static void Main(String[] args)
+        {
+            IPHostEntry ipHostInfo = Dns.GetHostEntry(Dns.GetHostName());
+            IPAddress ipAddress = ipHostInfo.AddressList[0];
+            IPEndPoint localEP = new IPEndPoint(ipAddress, _port);
+
+            var listener = new TcpListener(localEP);
+
+            listener.Start();
+            Console.WriteLine($"Server {localEP} started listening...");
+
+            bool done = false;
+            while (!done)
+            {
+                string message = DateTime.Now.ToString();
+                AcceptAndRespond(listener, message);
+            }
+            listener.Stop();
+            Console.WriteLine($"Server {localEP} stopped listening.");
+
+            Console.WriteLine("Press enter to exit program...");
+            Console.ReadLine();
+        }
+
+        static void AcceptAndRespond(TcpListener listener, string message)
+        {
+            Console.WriteLine("Waiting for connection...");
+            TcpClient client = listener.AcceptTcpClient();
+            Console.WriteLine($"Connection accepted from {client.Client.RemoteEndPoint}");
+            NetworkStream stream = client.GetStream();
+            byte[] byteTime = Encoding.ASCII.GetBytes(message);
+            try
+            {
+                stream.Write(byteTime, 0, byteTime.Length);
+                Console.WriteLine($"Message sent: \"{message}\" --> {client.Client.RemoteEndPoint} ");
+                Console.WriteLine($"");
+                stream.Close();
+                client.Close();
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e.Message);
+            }
+        }
+    }
+}
